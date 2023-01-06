54a978e5e37cf5a8128342b70e758a374f436e87
Spiriterkolo <spiriter.delabarre@gmail.com>
2023-01-06 08:49:36 +0100

    mask_maker now working properly

--- main.py
+++ main.py
@@ -3,13 +3,17 @@ import matplotlib.pyplot as plt
 
 #define the mask for the corresponding part where size is the image dimension (same for all images)
 #and part is the area of the mask that will contains ones
+#for 16 masks
 def mask_maker(size, indice):
+    if indice > indice*indice-1:
+      print("Mask index out of range")
+      return np.zeros(size)
     mask = np.zeros(size)
     indice_i = indice // 4
     indice_j = indice % 4
-    start_i = size[0] * int(indice_i / 4)
-    start_j = size[1] * int(indice_j / 4)
     offset = int(size[0]/4)
+    start_i = offset * int(indice_i)
+    start_j = offset * int(indice_j)
     mask[start_i:start_i+offset, start_j:start_j+offset] = np.ones((offset, offset))
     return mask
 
